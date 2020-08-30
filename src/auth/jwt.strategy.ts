eb07d41480021061bde1d95c4c2b2af37ed8534d
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-30 11:25:14 -0300

    feat: add user authentication and append it on request

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport";
-import { ExtractJwt } from "passport-jwt";
+import { Strategy, ExtractJwt } from "passport-jwt";
 import { JwtPayload } from "./jwt-payload.interface";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "./user.repository";
@@ -9,7 +8,7 @@ import { User } from "./user.entity";
 
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'JwtStrategy') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
